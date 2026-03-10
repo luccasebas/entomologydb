44fd271c12ac23e2ea8b42ce5ada10153a03d245
Lucca Sebastiani <lsebastiani@sandiego.edu>
2026-03-10 14:56:17 -0700

    redundant files deleted

--- map-page/map.js
+++
@@ -1,8 +0,0 @@
-const map = new maplibregl.Map({
-  container: 'map',
-  style: './style.json',
-  center: [-117.16, 32.71],
-  zoom: 10
-});
-
-map.addControl(new maplibregl.NavigationControl());
--- search-page/search.js
+++
@@ -1,7 +0,0 @@
-function toggleSection(element) {
-  const content = element.nextElementSibling;
-  const arrow = element.querySelector(".arrow");
-
-  content.classList.toggle("open");
-  arrow.classList.toggle("rotate");
-}
